6ea261c34364b0d5877851cb4a112f1ab792ca6f
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2021-12-24 18:38:50 +0530

    commit

---
+++ Project-React/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+import FormContainer from "./FormContainer";
+// import FormComponent from "./FormComponent";
+import './App.css';
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <FormContainer/>
+        {/* <FormComponent/> */}
+      </div>
+    )
+  }
+}
+export default App;
